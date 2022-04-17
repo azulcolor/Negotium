9ba31534b4b29c65fd77387abb7a61c7cb79f737
Pastelin <lealpastelin@gmail.com>
2022-04-16 19:48:05 -0500

    1.0.0

---
+++ src/routes/tasks.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../database');
+
+router.get('/add', async function (req, res) {
+    const users = await pool.query('SELECT * FROM usuario')
+    res.render('tasks/add', {users});
+})
+
+router.post('/add', async (req, res) => {
+    const { taskName, date, user, description, importance } = req.body;
+    const newTask = { 
+        
+        tarea:taskName, 
+        fechaExpiracion:date, 
+        idUsuario: user,
+        descripcion:description,
+        importancia:importance,
+        idStatus: 1
+    }
+    
+    await pool.query('INSERT INTO tareas set ?', [newTask]);
+    res.send('received');
+})
+
+router.get('/', async (req, res) => {
+    const tasks = await pool.query('SELECT * FROM tareas');
+    res.render('tasks/list', { tasks })
+})
+
+module.exports = router;
